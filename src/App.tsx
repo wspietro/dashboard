b540b32c766b84495fea7a4a73d669ec2556756d
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-04-04 18:08:56 -0300

    feat: shadcn ui config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from './components/ui/button'
+import './global.css'
 
 export function App() {
   return (
-    <h1>Hello dashboard!</h1>
+    <>
+      <div className='flex justify-center items-center h-screen'>
+        <Button>Enviar</Button>
+      </div>
+    </>
   )
 }
 
