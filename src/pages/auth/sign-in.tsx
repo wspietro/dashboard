708ad0f249949f323dacda722a5fe8926d385528
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-04-08 17:29:35 -0300

    config: sonner for toast and notification context

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -22,6 +23,8 @@ export function SignIn() {
   async function handleSignIn(data: SignInFormType) {
     console.log(data)
     await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    toast.success('Enviamos um link de autenticação para seu e-mail.')
   }
 
   return (
