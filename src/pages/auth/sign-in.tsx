db9ff021939fc11fbd6dfbf895ffc4f36a679966
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-04-16 16:14:18 -0300

    feat: sign in and sign up pages

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -31,6 +32,10 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button asChild className="absolute right-8 top-8" variant="ghost">
+          <Link to={'/sign-up'}>Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -42,7 +47,7 @@ export function SignIn() {
           </div>
           <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
-              <Label htmlFor="email">Seu e-mail</Label>
+              <Label htmlFor="email">Seu e-mail:</Label>
               <Input id="email" type="email" {...register('email')} />
             </div>
             <Button disabled={isSubmitting} className="w-full" type="submit">
