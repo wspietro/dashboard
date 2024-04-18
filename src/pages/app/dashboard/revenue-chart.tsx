c84042fe917dee60c9278a029ff89bfed8ea98f2
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-04-18 16:15:27 -0300

    feat: revenue chart w/ recharts

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,81 @@
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  {
+    data: '10/12',
+    revenue: 1200,
+  },
+  {
+    data: '11/12',
+    revenue: 1367,
+  },
+  {
+    data: '12/12',
+    revenue: 954,
+  },
+  {
+    data: '13/12',
+    revenue: 1090,
+  },
+  {
+    data: '14/12',
+    revenue: 765,
+  },
+  {
+    data: '15/12',
+    revenue: 1567,
+  },
+  {
+    data: '16/12',
+    revenue: 1478,
+  },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="data" tickLine={false} axisLine={false} dy={16} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+              width={80}
+            />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet[500]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
