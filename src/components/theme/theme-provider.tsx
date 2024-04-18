b537508c80664ee93e3379d836dfbc46469a8581
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-04-17 22:56:47 -0300

    docs: theme-provider comments

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -18,24 +18,27 @@ const initialState: ThemeProviderState = {
   setTheme: () => null,
 }
 
+// context do react
 const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 
 export function ThemeProvider({
   children,
   defaultTheme = 'system',
-  storageKey = 'vite-ui-theme',
+  storageKey = 'vite-ui-theme', // nome que será salvo no local-storage
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
+    // caso o usuário já tenha algo salvo no local-storage ou o tema default
     () => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
   )
 
   useEffect(() => {
-    const root = window.document.documentElement
+    const root = window.document.documentElement // busca o html do documento
 
-    root.classList.remove('light', 'dark')
+    root.classList.remove('light', 'dark') // remove todas as possíveis classe
 
     if (theme === 'system') {
+      // caso seja system, confere se existe a preferência dark
       const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
         .matches
         ? 'dark'
@@ -45,12 +48,13 @@ export function ThemeProvider({
       return
     }
 
-    root.classList.add(theme)
+    root.classList.add(theme) // se não for system, utiliza o theme definido
   }, [theme])
 
   const value = {
     theme,
     setTheme: (theme: Theme) => {
+      // retorna a função que atualiza o local storage e o state, que dispara o useEffect
       localStorage.setItem(storageKey, theme)
       setTheme(theme)
     },
@@ -64,6 +68,7 @@ export function ThemeProvider({
 }
 
 export const useTheme = () => {
+  // função helper para utilizarmos o contexto
   const context = useContext(ThemeProviderContext)
 
   if (context === undefined)
