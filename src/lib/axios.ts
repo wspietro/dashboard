0e89a19b4cf9053e166830f53b6b491092b83afe
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-04-19 17:04:47 -0300

    feat: skeleton loading state

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,13 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL, // url do backend
   withCredentials: true, // cookies do front são automaticamente enviados ao be
 })
+
+// criar delay para a aplicação, simulando um banco de dados em prod (local é muito rápido)
+// intercepta cada requisicao (corpo, cabeçalho, dados)
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
