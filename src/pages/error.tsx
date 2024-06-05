963bbcccf6a631def7e1ac310b802e430f991271
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-06-05 18:34:50 -0300

    feat: error and 404 pages

---
+++ src/pages/error.tsx
@@ -0,0 +1,24 @@
+import { Link, useRouteError } from 'react-router-dom'
+
+export function Error() {
+  // retorna o erro ocorrido
+  const error = useRouteError() as Error
+
+  // não é indicado mostrar o erro ao usuário
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">Whoops, algo aconteceu...</h1>
+      <p className="text-accent-foreground">
+        Um erro aconteceu na aplicação, abaixo você encontra mais detalhes:
+      </p>
+      <pre>{error.message || JSON.stringify(error)}</pre>
+      <p className="text-accent-foreground">
+        Voltar para o{' '}
+        <Link to="/" className="text-sky-400 dark:text-sky-500">
+          Dashboard
+        </Link>
+      </p>
+    </div>
+  )
+}
