0b4fd3a912feab22ee0e088b8eee77a235293700
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-06-23 14:11:03 -0300

    test-e2e: update profile

---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+
+test('update profile successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill('Rocket Pizza')
+  await page.getByLabel('Descrição').fill('Another Description')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle') // aguarda qq requisicão http finalizar
+
+  const toast = page.getByText('Perfil atualizado com sucesso!')
+
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+
+  await page.waitForTimeout(250) // aguardar fechamento do modal
+
+  expect(page.getByRole('button', { name: 'Rocket Pizza' })).toBeVisible()
+})
